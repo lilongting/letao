99136ea92c07d123f615191d7879daf2ee04c7ee
long <llt@163.com>
2017-10-29 21:19:18 +0800

    index基本框架完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,14 +2,22 @@
  * Created by msi on 2017/10/29.
  */
 //校验用户是否登录状态
-if(location.href.indexOf("login.html")<0){
+if (location.href.indexOf("login.html") < 0) {
   $.ajax({
-    type:"get",
-    url:"/employee/checkRootLogin",
-    success:function (data) {
-        if(data.error===400){
-          location.href ="login.html";
-        }
+    type: "get",
+    url: "/employee/checkRootLogin",
+    success: function (data) {
+      if (data.error === 400) {
+        location.href = "login.html";
+      }
     }
   })
 }
+$(document).ajaxStart(function () {
+  NProgress.start();
+})
+$(document).ajaxStart(function () {
+  setTimeout(function () {
+    NProgress.done();
+  },500);
+})
