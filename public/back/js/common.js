aeebb09d0f0fc64b7c5e59ee80cce4fa25f5f508
long <llt@163.com>
2017-10-29 22:18:27 +0800

    主页面的登陆功能实现

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -19,5 +19,29 @@ $(document).ajaxStart(function () {
 $(document).ajaxStart(function () {
   setTimeout(function () {
     NProgress.done();
-  },500);
+  }, 500);
+});
+//点击分类管理，显示或隐藏二级分类
+$('.child').prev().on("click", function () {
+  $(this).next().slideToggle();
+})
+//点击icon_menu，显示或隐藏侧边栏
+$(".icon_menu").on("click", function () {
+  $(".lt_aside").toggleClass("now");
+  $(".lt_main").toggleClass("now");
+})
+//退出功能
+$(".icon_logout").on("click", function () {
+  $("#logoutModal").modal("show");
+})
+$(".btn_logout").on("click", function () {
+  $.ajax({
+    type: "get",
+    url: "/employee/employeeLogout",
+    success: function (data) {
+      if (data.success) {
+        window.location.href = "login.html";
+      }
+    }
+  })
 })
