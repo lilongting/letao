2ce0ba282a7c737bea33fed982cafd7e1b3d976d
long <llt@163.com>
2017-11-01 20:39:42 +0800

    移动端主页已完成以及少许分类页

---
+++ public/front/js/category.js
@@ -0,0 +1,8 @@
+/**
+ * Created by msi on 2017/11/1.
+ */
+// 开启滚动 左右滚动 是数组
+var sc = mui(".lt_category_l .mui-scroll-wrapper").scroll({
+  deceleration: 0.0005,
+  indicators: false
+})
---
+++ public/front/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by msi on 2017/11/1.
+ */
+//区域滚动
+mui(".mui-scroll-wrapper").scroll({
+  indicators: false
+});
+
+//轮播图
+mui(".mui-slider").slider({
+  interval:1000
+});
